86b6cf11862cbaecd22940bdc1079c2dadeb9d31
linmaliakal <lmaliakal@luc.edu>
2017-09-12 04:24:49 +0000

    lab1-part4
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/lab1/FizzBuzzPart4.java
+++ src/main/java/lab1/FizzBuzzPart4.java
@@ -2,7 +2,7 @@ package lab1;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-//simply copy part 3 but use List/ArrayList from Java Collections Library instead of an array
+//copy part 3 but use List/ArrayList from Java Collections Library instead of an array
 public class FizzBuzzPart4 {
 
     public static void main(String[] args) {
@@ -10,34 +10,37 @@ public class FizzBuzzPart4 {
 
         System.out.println("Please enter a positive integer (this will be the number the sequence will go up to):  ");
         int i = scan.nextInt();
-        if (i <= 0){
-            System.out.println("I am sorry, but " + i + " is not a positive integer.");
-        }
         
-        int i = 20;
-        System.out.println("Goes up to " + i);
+        ArrayList<String> fizzBuzzResult = new ArrayList<String>();
+        fizzBuzzResult = FizzBuzz(i, fizzBuzzResult);
 
-        for (int k=1; k<=i; k++){
-
-            if ((k % 3 == 0) && (k % 5 == 0)){
-                System.out.println("Fizz-Buzz");
-            }
-            else if (k % 3 == 0){
-                System.out.println("Fizz");
-            }
-            else if (k % 5 == 0){
-                System.out.println("Buzz");
-            }
-            else{
-                System.out.println(k);
-            }
-
+        for (int n = 1; n < fizzBuzzResult.size(); n++){
+            System.out.println(fizzBuzzResult.get(n));
         }
 
     }
     
-    public static ArrayList<String> fizzBuzzArrayList(final int n) {
-        //no need to include restrictions for List
+    public static ArrayList<String> fizzBuzzArrayList(final int i, String[] fizzBuzzResult) {
         //simply using List from Java collections library instead of Array
+        for (int k=1; k<=(i+1); k++){
+
+            if ((k % 3 == 0) && (k % 5 == 0)){
+                fizzBuzzResult.add("Fizz-Buzz");
+            }
+            else if (k % 3 == 0){
+                fizzBuzzResult.add("Fizz");
+            }
+            else if (k % 5 == 0){
+                fizzBuzzResult.add("Buzz");
+            }
+            else{
+                fizzBuzzResult.add(String.valueOf(k));
+            }
+
+        }
+        
+        System.out.println(ArrayList.toString(fizzBuzzResult));
+        
+        return fizzBuzzResult;
     }
 }
