bfb164bc963efb441fc0c585d30d06b5eedc7b1e
linmaliakal <lmaliakal@luc.edu>
2017-09-12 03:42:06 +0000

    lab1-part3
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/lab1/FizzBuzzPart3.java
+++ src/main/java/lab1/FizzBuzzPart3.java
@@ -11,35 +11,41 @@ public class FizzBuzzPart3 {
 
         System.out.println("Please enter a positive integer (this will be the number the sequence will go up to):  ");
         int i = scan.nextInt();
-        if (i <= 0){
-            System.out.println("I am sorry, but " + i + " is not a positive integer.");
-        }
         
-        int i = 20;
-        System.out.println("Goes up to " + i);
+        String[] fizzBuzzOutput = new String[n+1];
+        fizzBuzzOutput = FizzBuzz(n, fizzBuzzOutput);
 
+        for (int i = 1; i < fizzBuzzOutput.length; i++){
+            System.out.println(fizzBuzzOutput[i]);
+        }
+        //if (i <= 0){
+          //  System.out.println("I am sorry, but " + i + " is not a positive integer.");
+       // }
+    }
+    
+    public static String[] FizzBuzz(final int n, String[] fizzBuzzOutput){
+        //needs to store resulting data in an Array and return that array
+        //have test suite for values of n: -10, -1, 0, 1, 7, 17
         for (int k=1; k<=i; k++){
 
             if ((k % 3 == 0) && (k % 5 == 0)){
-                System.out.println("Fizz-Buzz");
+                fizzBuzzOutput[k] = "Fizz-Buzz";
             }
             else if (k % 3 == 0){
-                System.out.println("Fizz");
+                fizzBuzzOutput[k] = "Fizz";
             }
             else if (k % 5 == 0){
-                System.out.println("Buzz");
+                fizzBuzzOutput[k] = "Buzz";
             }
             else{
-                System.out.println(k);
+                fizzBuzzOutput[k] = String.valueOf[k];
             }
 
         }
-
-    }
-    
-    public static String[] FizzBuzz(final int n){
         
-        //needs to store resulting data in an Array and return that array
-        //have test suite for values of n: -10, -1, 0, 1, 7, 17
+        System.out.println(Arrays.toString(fizzBuzzOutput));
+        
+        return fizzBuzzOutput;
+        
     }
 }
