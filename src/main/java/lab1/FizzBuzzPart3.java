c719fb87b9dfc9250028cea9bef2c16b5971642e
linmaliakal <lmaliakal@luc.edu>
2017-09-18 03:44:25 +0000

    lab1-part3 google java format
    Signed-off-by: linmaliakal <lmaliakal@luc.edu>

--- src/main/java/lab1/FizzBuzzPart3.java
+++ src/main/java/lab1/FizzBuzzPart3.java
@@ -6,26 +6,23 @@ public class FizzBuzzPart3 {
 
     public static void main(String[] args) {
         //should be responsible only for converting command line argument to a number & passing it to FizzBuzz method
-        //then printing the resulting Array
-        Scanner scan = new Scanner(System.in);
+        //Scanner scan = new Scanner(System.in);
 
         System.out.println("Please enter a positive integer (this will be the number the sequence will go up to):  ");
-        int i = scan.nextInt();
+        //int i = scan.nextInt();
+        int i = 20;
         
         String[] fizzBuzzOutput = new String[i+1];
         fizzBuzzOutput = FizzBuzz(i, fizzBuzzOutput);
 
         for (int n = 1; n < fizzBuzzOutput.length; n++){
+            
             System.out.println(fizzBuzzOutput[i]);
         }
-        //if (i <= 0){
-          //  System.out.println("I am sorry, but " + i + " is not a positive integer.");
-       // }
     }
     
     public static String[] FizzBuzz(final int i, String[] fizzBuzzOutput){
         //needs to store resulting data in an Array and return that array
-        //have test suite for values of n: -10, -1, 0, 1, 7, 17
         for (int k=1; k<=i; k++){
 
             if ((k % 3 == 0) && (k % 5 == 0)){
